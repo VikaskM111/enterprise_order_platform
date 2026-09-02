d8d5ceb8b76b2f5f62fd37b3ddc9de16181f49a2
vikash <vikash.khalari@gmail.com>
2026-09-03 00:29:36 +0530

    project foundation setUp

---
+++ src/main/java/com/platform/enterpriseorderplatform/EnterpriseOrderPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.platform.enterpriseorderplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseOrderPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseOrderPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/platform/enterpriseorderplatform/EnterpriseOrderPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.enterpriseorderplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseOrderPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
